f5be2b237e7071013eb6620a37b999fa1d6183e7
shubhamaydas1@gmail.com <shubhamaydas1@gmail.com>
2022-04-21 10:54:35 +0200

    Initial Commit

---
+++ CoordinatorPatternExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoordinatorPatternExample
+//
+//  Created by Shubhamay Das on 21/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
